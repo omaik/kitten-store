ef9df4f15ac108be45723b50086a68556b3d9577
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2021-09-09 09:35:35 +0300

    master: add versioning

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,1 +1,1 @@
- helm upgrade -i kitten-store ./ops/charts/kitten-chart -f ops/deployment/values.yml --wait
+ helm upgrade -i kitten-store ./ops/charts/kitten-chart -f ops/deployment/values.yml --debug --wait --set "app.version=$VERSION"
