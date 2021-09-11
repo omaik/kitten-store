83e83d2ad0129598b9eb2faf733e1c0ef304feaf
Oleh Maikovych <18499500+omaik@users.noreply.github.com>
2021-09-11 12:54:45 +0300

    master:

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,1 +1,7 @@
- helm upgrade -i kitten-store ./ops/charts/kitten-chart -f ops/deployment/values.yml --debug --wait --set "app.version=$VERSION"
+ helm upgrade -i kitten-store ./ops/charts/kitten-chart \
+  -f ops/deployment/values.yml \
+  --debug \
+  --wait \
+  --set "app.version=$VERSION" \
+  --set "app.image=$DOCKER_IMAGE" \
+  --set "database_url=$DATABASE_URL" \
